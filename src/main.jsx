9dc513a5491f4b284c0bd733b59eb222d5d105aa
ASHISH KUMAR DUBEY <126683211+a-shish07@users.noreply.github.com>
2024-02-09 00:24:42 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   domain="dev-hnlvj0h7ml58eim1.us.auth0.com"
   clientId="D9LIgXhe3q5oS5qsAjv3Gb0WL0Nn8Pe8"
   authorizationParams={{
-    redirect_uri:  https://a-shish07.github.io
+    redirect_uri:  https://a-shish07.github.io/Travel_Website/
   }}>
   <App />
 </Auth0Provider>,
